906e995f089da732cddb5e00be0eca8a41cb51af
Etienne Le Bourlout <etienne.le-bourlout@hei.yncrea.Fr>
2017-11-24 08:26:37 +0100

    souligne le text

--- Pools/app/src/main/java/com/example/hippolyte/pools/PoolDetails.java
+++ Pools/app/src/main/java/com/example/hippolyte/pools/PoolDetails.java
@@ -1,7 +1,12 @@
 package com.example.hippolyte.pools;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +21,7 @@ public class PoolDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_pool_details);
 
         tvNom= (TextView)findViewById(R.id.nomPiscine);
         tvAdresse= (TextView)findViewById(R.id.urlVille);
@@ -29,12 +34,33 @@ public class PoolDetails extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         tvNom.setText(bundle.getString("libelle"));
         tvVille.setText(bundle.getString("ville"));
-        tvAdresse.setText(bundle.getString("url"));
+
+        //souligne l'url
+        SpannableString content = new SpannableString(bundle.getString("url"));
+        content.setSpan(new UnderlineSpan(), 0, bundle.getString("url").length(), 0);
+        tvAdresse.setText(content);
+
         tvCP.setText(bundle.getString("cp"));
         tvPtX.setText(bundle.getString("ptX"));
         tvPtY.setText(bundle.getString("ptY"));
 
         //a enlever a la fin
         Toast.makeText(this, tvPtX.getText()+" -- "+tvPtY.getText(), Toast.LENGTH_SHORT).show();
+
+        //permet de rechercher l'url dans le navigateur
+        tvAdresse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String link;
+                link = tvAdresse.getText().toString();
+                if (!(link.startsWith("http://") || link.startsWith("https://"))){
+                    link="http://"+link;
+                }
+                Uri WebUrl = Uri.parse(link);
+                Intent lancerNavigateur = new Intent(Intent.ACTION_VIEW);
+                lancerNavigateur.setData(WebUrl);
+                startActivity(lancerNavigateur);
+            }
+        });
     }
 }
